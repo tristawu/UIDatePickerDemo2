35b46cc970b0978083b7b8d71018003e63088acd
TristaWu <trista@kcdgs.com>
2021-02-04 15:44:21 +0800

    增加與 在ViewController 建立 UI版 的作法 比較

--- UIDatePickerDemo2/ViewController.swift
+++ UIDatePickerDemo2/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /*建立兩個屬性,同
+     var myDatePicker: UIDatePicker!
+     var myLabel: UILabel!
+     */
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //設置 NSDate 的格式
+        let formatter = DateFormatter()
+        
+        //設置時間顯示的格式
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+
+        //設置顯示的語言環境
+        myDatePicker.locale = NSLocale(localeIdentifier: "zh_TW") as Locale
+        
     }
 
-
+    /*設置改變日期時間時會執行動作的方法,同
+     myDatePicker.addTarget(self,action:#selector(ViewController.datePickerChanged),for: .valueChanged)
+    */
+    @IBAction func datePickerChanged(_ sender: Any) {
+        //設置顯示在 UILabel 的日期時間格式
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+    
+        //更新 UILabel 的內容
+        myLabel.text = formatter.string(from: (sender as AnyObject).date)
+    }
+    
 }
 
